32d21e921534aa55ad5ccb71dd09d965cf0cee08
sumukshashidhar <sumukuuu@gmail.com>
2020-05-22 11:45:35 +0530

    added registering and signup

--- index.js
+++ index.js
@@ -7,13 +7,12 @@ const cookieParser = require('cookie-parser');
 const mongoose = require("mongoose");
 const db = require('./config/database.js');
 import { nanoid } from 'nanoid';
-
-
-const PORT = 3000;
+const client = require('./models/client.js')
+const JWT_OPTIONS = require('./config/jwt.js')
 
 var app = express();
 mongoose.Promise = global.Promise; 
-
+app.use(express.static("styles"));
 
 mongoose.connect(db.mongoURI, {
     useNewUrlParser: true, 
@@ -44,9 +43,56 @@ app.get('/', function(res, req) {
 });
 
 
-app.post('/login', function(req, res) {
+app.get('/login', function(req, res){
+	if(req.cookies.access_token!=undefined){
+	  jwt.verify(req.cookies.access_token, publicKEY, JWT_OPTIONS.verifyOptions, function(err, decodedToken){
+		if(err){
+		  console.log(err)
+		  res.clearCookie('access_token').redirect('/login')
+		}
+		else{
+			res.redirect('/home')
+		}
+	  })
+	}
+	else{
+	  res.render('login')
+	}
+  })
 
-})
+  app.post('/login', function(req, res){
+	user.findOne({DisplayName: req.body.DisplayName}, function(err, USER_OBJ){
+	  if(err){
+		console.log(err)
+	  }
+	  else{
+		bcrypt.compare(req.body.password, USER_OBJ.password, function(err, BCRYPT_RES){
+		  if(err){
+			console.log(err)
+		  }
+		  else{
+			if(BCRYPT_RES==true){
+			  var token = jwt.sign({nickname: req.body.DisplayName, id: USER_OBJ._id, role: USER_OBJ.userType}, privateKEY, JWT_OPTIONS.signOptions)
+			  console.log("jwt generated")
+			  res.cookie('access_token', token, {expires: new Date(Date.now() + 24 * 60 * 60 * 1000)})
+			  activeUsers = activeUsers + 1;
+			  if(USER_OBJ.userType == 'admin'){
+				res.redirect('/admindash')
+			  }
+			  else{
+				res.redirect('/home')
+			  }
+  
+			}
+			else{
+			  console.log("Authentication Failed")
+			  res.redirect('/login')
+			}
+		  }
+		})
+	  }
+	})
+  })
 
 app.post('/register', function(req, res){
 	bcrypt.hash(req.body.password, 9, function(err, BCRYPT_HASH){
@@ -68,13 +114,13 @@ app.post('/register', function(req, res){
 		  }
 		  else{
 			console.log("Successful User Registration. Proceed to login")
+			console.log(obj.key)
 			console.log(obj)
 			res.redirect('/login')
 		  }
 		})
 	  }
 	})
-  
   })
   
 
